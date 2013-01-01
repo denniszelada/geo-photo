5fb2df703d6efee4990d35b4980c2da39c35d5c6
Dennis Zelada <denniszelada@gmail.com>
2013-01-01 03:28:18 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoPhoto::Application.config.secret_token = 'ab0b765929020eed5cb732b83f59763a35d4aaab5f48842e7139492f4007954a0f6f6364ac54afff09712caae5608fcda6c11c83783235311433b53ed32b3acb'
